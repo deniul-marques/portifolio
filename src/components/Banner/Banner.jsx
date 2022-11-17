6f57e14c874c0cad9b2136c46a5ad65d09c0c345
Deniul Marques dos Santos <deniulmarques-10@hotmail.com>
2022-11-17 12:04:25 -0300

    Cursos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,127 @@
-import {BsArrowRightShort} from "react-icons/bs";
+import { BsArrowRightShort } from "react-icons/bs";
 
 export default function Banner() {
       return (
             <div className="w-4/5 flex-auto h-min ">
                   <div className="bg-white">
-                      <div className="px-16 py-20">
-                          <div className="grid grid-cols-2">
-                              <div className="space-y-6">
-                                  <h1 className="font-semibold text-5xl">Eu sou Deniul Marques<br /><span className="text-[#FFB400]">Front-end</span> Developer </h1>
-                                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
-                                  <button className="flex rounded items-center uppercase bg-[#FFB400] px-4 py-1" href="#">Hire Me <span className="text-2xl"><BsArrowRightShort /></span></button>
+                        <div className="px-16 py-20">
+                              <div className="space-y-5">
+                                    <h1 className="font-semibold text-5xl">Eu sou Deniul Marques<br /><span className="text-[#FFB400]">Front-end</span> Developer </h1>
+                                    <p className="max-w-[50ch]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, volutpat feugiat placerat lobortis. Natoque rutrum semper sed suspendisse nunc lectus.</p>
+                                    <button className="flex rounded items-center uppercase bg-[#FFB400] px-4 py-1" href="#">Hire Me <span className="text-2xl"><BsArrowRightShort /></span></button>
                               </div>
-                          </div>
-                      </div>
+                        </div>
                   </div>
-                
-                <div className="flex flex-col justify-center items-center my-10 text-center gap-5">
-                    <h2 className="text-2xl font-bold underline underline-offset-8">Meus Serviços</h2>
-                    <p className="max-w-[45ch]">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
-                </div>
+
+                  <div className="flex flex-col justify-center items-center my-10 text-center gap-5">
+                        <h2 className="text-2xl font-bold underline underline-offset-8">Meus Serviços</h2>
+                        <p className="max-w-[45ch]">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
+                  </div>
+
+                  <div className="bg-white flex flex-col items-center p-5 gap-2">
+                        <img className="h-20 w-20" src="https://liarmorb.sirv.com/images/coding.png" alt="" />
+                        <p className="font-semibold capitalize text-lg">web development</p>
+                        <span className="text-gray-500 text-sm capitalize">blog, landing page</span>
+                  </div>
+
+                  <div className="flex flex-col justify-center items-center my-10 text-center gap-5">
+                        <h2 className="text-2xl font-bold underline underline-offset-8">Cursos</h2>
+                  </div>
+
+                  <div className="bg-white px-14 ">
+                        <ul className="divide-y">
+                              <li className="flex gap-10 p-8  justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Universidade Estácio de Sá</h3>
+                                          <p className="text-gray-600 text-sm">Cursando <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>Sistemas de Informação</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>Django - Formulários, requisições e mensagens</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>Django - Modelo, rotas e views</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>Django - Filtros, Buscas e Admin</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>Flexbox - Posicione elementos na tela</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>HTML 5 e CSS - Crie uma página na WEB</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>HTML 5 e CSS - Posicionamento, listas e navegação</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>HTML 5 e CSS - Formulários e Tabelas</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>HTML 5 e CSS - CSS Avançado</h2>
+                                    </div>
+                              </li>
+                              <li className="flex gap-10 p-8 justify-between items-center">
+                                    <div>
+                                          <h3 className="text-md font-semibold">Alura</h3>
+                                          <p className="text-gray-600 text-sm">Concluido <span className="bg-[#FFB400] text-white px-2 p-y">8º Período</span></p>
+                                    </div>
+                                    <div>
+                                          <h2>HTML 5 e CSS - CSS Avançado</h2>
+                                    </div>
+                              </li>
+                        </ul>
+                  </div>
             </div>
       );
 };
